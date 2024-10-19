480a95360cc0e921ea7426dc3f6359d7fe38f6d6
Miguel Angel <migueel26@gmail.com>
2024-10-19 10:51:05 +0200

    error de sintaxis

--- src/components/Admin/Auth/RegisterForm/RegisterForm.form.js
+++ src/components/Admin/Auth/RegisterForm/RegisterForm.form.js
@@ -12,12 +12,12 @@ export function initialValues() {
 export function validationSchema() {
   return Yup.object({
     email: Yup.string()
-    .email("El email no es válido").
-    required("Campo obligatorio"),
-    password: Yup.string().required("Campo obligatorio"), 
+      .email("El email no es válido")
+      .required("Campo obligatorio"),
+    password: Yup.string().required("Campo obligatorio"),
     repeatPassword: Yup.string()
-    .required("Campo obligatorio")
+      .required("Campo obligatorio")
       .oneOf([Yup.ref("password")], "Las contraseñas tienen que ser iguales"),
-    conditionsAccepted: Yup.bool().isTrue(true), 
+    conditionsAccepted: Yup.bool().isTrue(true),
   });
 }
